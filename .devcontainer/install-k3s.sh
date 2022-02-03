979faf1d1c385b434979e88414379f8127e17250
Michael Hansen <Michael.Hansen@microsoft.com>
2022-02-03 21:49:15 +0000

    Merged PR 60637: Remove traefik from tyger helm chart
    With this PR, traefik is now installed with the k3s cluster and we add additional configuration to it to have the same logging settings as we did when installing with the tyger chart. Traefik is removed from tyger helm chart.
    
    > Note: You need to remove your k3s install before testing this to have it brought up in the right state.

--- .devcontainer/install-k3s.sh
+++ .devcontainer/install-k3s.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
 set -euo pipefail
-curl -sfL https://get.k3s.io | sh -s - --docker --no-deploy traefik --write-kubeconfig-mode 600
-sudo chown "$USER:$USER" /etc/rancher/k3s/k3s.yaml
+curl -sfL https://get.k3s.io | sh -s - --docker --write-kubeconfig-mode 600
+sudo chown "$USER:$(user -g)" /etc/rancher/k3s/k3s.yaml
+
+# Here we add any additional config changes to traefik
+chart_config=$(cat << EOF
+apiVersion: helm.cattle.io/v1
+kind: HelmChartConfig
+metadata:
+  name: traefik
+  namespace: kube-system
+spec:
+  valuesContent: |-
+    logs:
+      general:
+        format: "json"
+      access:
+        enabled: "true"
+        format: "json"
+EOF
+)
+
+echo "$chart_config" | sudo tee /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
+
+
+# Make sure changes take effect
+sudo systemctl restart k3s
