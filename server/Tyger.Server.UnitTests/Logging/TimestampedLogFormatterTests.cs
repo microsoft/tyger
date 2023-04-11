a46288c293bab864425791e45ef621ade3261dc6
John Stairs <jostairs@microsoft.com>
2023-04-11 22:42:33 +0000

    Merged PR 71750: Upgrade AKS k8s, kubectl, and Helm versions
    The Kubernetes version we were using is no longer supported by AKS.
    
    Update: Also needed to remove a workaround we had put in for handling long log lines. This was fixed in k8s [here](https://github.com/kubernetes/kubernetes/pull/113481).

--- server/Tyger.Server.UnitTests/Logging/TimestampedLogFormatterTests.cs
+++ server/Tyger.Server.UnitTests/Logging/TimestampedLogFormatterTests.cs
@@ -54,30 +54,6 @@ public class TimestampedLogFormatterTests
     }
 
     [Fact]
-    public async Task LongLines()
-    {
-        var input = "2022-04-18T13:34:38.519160930Z " + new string('a', 0x4000) + "2022-04-18T13:34:39.519160930Z " + new string('a', 0x60) + "\n" +
-                    "2022-04-18T13:34:40.519160930Z " + new string('b', 0x4000) + "2022-04-18T13:34:40.519160930Z " + new string('b', 0x4000) + "2022-04-18T13:34:41.519160930Z " + new string('b', 0x1);
-        var expected = "2022-04-18T13:34:38.519160930Z " + new string('a', 0x4060) + "\n" +
-                        "2022-04-18T13:34:40.519160930Z " + new string('b', 0x8001);
-
-        var pipeline = new Pipeline(Encoding.UTF8.GetBytes(input), new TimestampedLogReformatter());
-        (await pipeline.ReadAllAsString()).ShouldBe(expected);
-    }
-
-    [Fact]
-    public async Task LongLinesWithTrailingNewline()
-    {
-        var input = "2022-04-18T13:34:38.519160930Z " + new string('a', 0x4000) + "2022-04-18T13:34:39.519160930Z " + new string('a', 0x60) + "\n" +
-                    "2022-04-18T13:34:40.519160930Z " + new string('b', 0x4000) + "2022-04-18T13:34:40.519160930Z " + new string('b', 0x4000) + "2022-04-18T13:34:41.519160930Z " + new string('b', 0x1) + "\n";
-        var expected = "2022-04-18T13:34:38.519160930Z " + new string('a', 0x4060) + "\n" +
-                        "2022-04-18T13:34:40.519160930Z " + new string('b', 0x8001) + "\n";
-
-        var pipeline = new Pipeline(Encoding.UTF8.GetBytes(input), new TimestampedLogReformatter());
-        (await pipeline.ReadAllAsString()).ShouldBe(expected);
-    }
-
-    [Fact]
     public async Task LineWithoutTimestamp()
     {
         var input = @"unable to retrieve container logs for containerd://90cf2459f602ed64a3898dba30f698b6abf8e11f3ab0d12f1a570a9e7ce213d3";
