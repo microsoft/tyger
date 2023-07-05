1eed95ea5ecfa3fa72eef3da18897311451c17ab
John Stairs <jostairs@microsoft.com>
2023-07-05 18:51:33 +0000

    Merged PR 73022: Fix use-current-credentials.sh
    The script did not work if the kubectl config had no existing contexts.

--- scripts/use-current-credentials.sh
+++ scripts/use-current-credentials.sh
@@ -44,7 +44,7 @@ environment_definition=$(cat "${config_path}")
 
 primary_cluster_name=$(echo "${environment_definition}" | jq -r '.primaryCluster')
 
-context_name=$(kubectl config view -o json | jq -r --arg cluster_name "${primary_cluster_name}" '.contexts[] | select(.context.cluster == $cluster_name and (.context.user | startswith("clusterUser"))).name')
+context_name=$(kubectl config view -o json | jq -r --arg cluster_name "${primary_cluster_name}" '.contexts[]? | select(.context.cluster == $cluster_name and (.context.user | startswith("clusterUser"))).name')
 if [[ -z "${context_name}" ]]; then
     az aks get-credentials -n "${primary_cluster_name}" -g "$(echo "${environment_definition}" | jq -r '.resourceGroup')" --subscription="$(echo "${environment_definition}" | jq -r '.subscription')" --overwrite-existing
     kubelogin convert-kubeconfig -l azurecli
