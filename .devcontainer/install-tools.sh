c5c4f94fe2ec8c68901f4bd68b5421bb6b060a28
John Stairs <jostairs@microsoft.com>
2023-12-14 22:20:53 +0000

    Update psql version

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -22,6 +22,9 @@ sudo az aks install-cli --kubelogin-version "${KUBELOGIN_VERSION}" --install-loc
 # install dotnet-symbol
 dotnet tool install dotnet-symbol --global
 
+# install psql
+echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
-  postgresql-client
+  postgresql-client-16
