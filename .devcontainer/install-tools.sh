8cef27981c839825f8339f1a7809c7cfe731d742
John Stairs <jostairs@microsoft.com>
2024-01-17 17:21:31 +0000

    Support aarch64 devcontainer

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -4,16 +4,17 @@ set -euo pipefail
 
 # yq
 YQ_VERSION=v4.35.1
-YQ_BINARY=yq_linux_amd64
+YQ_BINARY="yq_linux_$(dpkg --print-architecture)"
 
-wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - |\
-  tar xz && mv ${YQ_BINARY} /usr/bin/yq
+wget "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz" -O - |\
+  tar xz && mv "${YQ_BINARY}" /usr/bin/yq
 
 # tv
 TV_VERSION="v0.7.0"
-wget "https://github.com/uzimaru0000/tv/releases/download/${TV_VERSION}/tv-x86_64-unknown-linux-gnu.zip" \
-&& unzip tv-x86_64-unknown-linux-gnu.zip \
-&& mv tv-x86_64-unknown-linux-gnu/tv /usr/bin
+TV_ARCHIVE="tv-$(uname -m)-unknown-linux-gnu"
+wget "https://github.com/uzimaru0000/tv/releases/download/${TV_VERSION}/${TV_ARCHIVE}.zip" \
+&& unzip "${TV_ARCHIVE}".zip \
+&& mv "${TV_ARCHIVE}/tv" /usr/bin
 
 # install kubelogin
 KUBELOGIN_VERSION=0.0.33
