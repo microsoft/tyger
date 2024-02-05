22b693b8f28752b2da455206b2099a7b9648a3db
John Stairs <jostairs@microsoft.com>
2024-02-05 21:50:46 +0000

    WIP

---
+++ server/Tyger.Server/Buffers/IBufferProvider.cs
@@ -0,0 +1,9 @@
+namespace Tyger.Server.Buffers;
+
+public interface IBufferProvider
+{
+    Task CreateBuffer(string id, CancellationToken cancellationToken);
+    Task<bool> BufferExists(string id, CancellationToken cancellationToken);
+
+    Task<Uri> CreateBufferAccessUrl(string id, bool writeable, CancellationToken cancellationToken);
+}
