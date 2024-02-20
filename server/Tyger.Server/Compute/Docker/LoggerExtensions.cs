1c6a28342c2a9cf046e2ed8f73f050f131a4f8d4
John Stairs <jostairs@microsoft.com>
2024-02-20 20:36:24 +0000

    Support run cancel

--- server/Tyger.Server/Compute/Docker/LoggerExtensions.cs
+++ server/Tyger.Server/Compute/Docker/LoggerExtensions.cs
@@ -6,7 +6,10 @@ public static partial class LoggerExtensions
     [LoggerMessage(0, LogLevel.Warning, "Failed to remove container")]
     public static partial void FailedToRemoveContainer(this ILogger logger, string containerId, Exception exception);
 
-    [LoggerMessage(1, LogLevel.Warning, "Failed to remove secrets for run {runId}")]
+    [LoggerMessage(1, LogLevel.Warning, "Failed to kill container")]
+    public static partial void FailedToKillContainer(this ILogger logger, string containerId, Exception exception);
+
+    [LoggerMessage(2, LogLevel.Warning, "Failed to remove secrets for run {runId}")]
     public static partial void FailedToRemoveRunSecretsDirectory(this ILogger logger, long runId, Exception exception);
 
 }
