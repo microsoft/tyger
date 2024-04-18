58f56e6289396f06026e6e94fd96f1b4502a49df
John Stairs <jostairs@microsoft.com>
2024-04-18 21:22:34 +0000

    Update devcontainer bind mounts

--- .devcontainer/prepare-host.sh
+++ .devcontainer/prepare-host.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
+uid=$(id -u)
+gid=$(id -g)
+
 # Check if /opt/tyger exists. If if does, exit. Otherwise, create it. This will require sudo.
 # The ownership should be the same as if it sudo hasn't been used.
 
 if [ ! -d /opt/tyger ]; then
-    uid=$(id -u)
-    gid=$(id -g)
-
     sudo mkdir /opt/tyger
     sudo chown -R "$uid":"$gid" /opt/tyger
+fi
+
+if [ ! -d /tmp/tyger ]; then
+    mkdir -m 777 /tmp/tyger
 fi
