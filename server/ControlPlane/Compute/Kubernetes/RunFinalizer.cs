a4b3a158df78de97a4aec97b89841e722cfa6443
Michael Hansen <mihansen@microsoft.com>
2025-07-18 16:20:48 +0000

    PR feedback

--- server/ControlPlane/Compute/Kubernetes/RunFinalizer.cs
+++ server/ControlPlane/Compute/Kubernetes/RunFinalizer.cs
@@ -198,16 +198,8 @@ public class RunFinalizer : BackgroundService
             }
         }
 
-        await _repository.UpdateRunAsFinal(runState.Id, cancellationToken);
-        var runResult = await _repository.GetRun(runState.Id, cancellationToken, GetRunOptions.SkipTags);
-        if (runResult != null)
-        {
-            _logger.FinalizedRun(runState.Id, runState.Status.ToString(), runResult.Value.run.CreatedAt, runResult.Value.run.StartedAt, runResult.Value.run.FinishedAt);
-        }
-        else
-        {
-            _logger.FinalizedRun(runState.Id, runState.Status.ToString(), null, null, null);
-        }
+        var (createdAt, startedAt, finishedAt) = await _repository.UpdateRunAsFinal(runState.Id, cancellationToken);
+        _logger.FinalizedRun(runState.Id, runState.Status.ToString(), createdAt, startedAt, finishedAt);
     }
 
     private async Task ArchiveLogs(long runId, CancellationToken cancellationToken)
