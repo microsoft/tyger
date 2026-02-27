4584edb917915b80b0be9b988feeda42ff7425dc
John Stairs <jostairs@microsoft.com>
2026-02-27 19:59:25 +0000

    OpenApi fixes

--- server/ControlPlane/OpenApi/OpenApi.cs
+++ server/ControlPlane/OpenApi/OpenApi.cs
@@ -2,11 +2,11 @@
 // Licensed under the MIT License.
 
 using System.Reflection;
+using System.Text.Json.Nodes;
 using Asp.Versioning.ApiExplorer;
 using k8s.Models;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Tyger.Common.Versioning;
 using Tyger.ControlPlane.Model;
@@ -28,7 +28,7 @@ public static class OpenApi
             c.UseOneOfForPolymorphism();
             c.UseAllOfForInheritance();
 
-            c.MapType<CodespecKind>(() => new OpenApiSchema { Enum = [new OpenApiString("job"), new OpenApiString("worker")] });
+            c.MapType<CodespecKind>(() => new OpenApiSchema { Enum = [JsonValue.Create("job"), JsonValue.Create("worker")] });
             c.SelectDiscriminatorNameUsing(type => type == typeof(JobCodespec) ? "kind" : null);
             c.SelectDiscriminatorValueUsing(type => type switch
             {
@@ -37,14 +37,14 @@ public static class OpenApi
                 _ => null
             });
 
-            c.MapType<RunKind>(() => new OpenApiSchema { Enum = [new OpenApiString("user"), new OpenApiString("system")] });
+            c.MapType<RunKind>(() => new OpenApiSchema { Enum = [JsonValue.Create("user"), JsonValue.Create("system")] });
 
-            c.MapType<ResourceQuantity>(() => new OpenApiSchema { Type = "string" });
-            c.MapType<CommittedCodespecRef>(() => new OpenApiSchema { Type = "string" });
+            c.MapType<ResourceQuantity>(() => new OpenApiSchema { Type = JsonSchemaType.String });
+            c.MapType<CommittedCodespecRef>(() => new OpenApiSchema { Type = JsonSchemaType.String });
             c.MapType<RunStatus>(() => new OpenApiSchema
             {
-                Type = "string",
-                Enum = [.. typeof(RunStatus).GetEnumNames().Select(value => new OpenApiString(value))]
+                Type = JsonSchemaType.String,
+                Enum = [.. typeof(RunStatus).GetEnumNames().Select(value => (JsonNode)JsonValue.Create(value))]
             });
 
             c.SelectSubTypesUsing(type =>
@@ -71,6 +71,7 @@ public static class OpenApi
             c.SchemaFilter<OpenApiExcludeSchemaFilter>();
 
             c.OperationFilter<ApiVersionParameterFilter>();
+            c.OperationFilter<TagsQueryParameterOperationFilter>();
 
             var filePath = Path.Combine(AppContext.BaseDirectory, "tyger-server.xml");
             c.IncludeXmlComments(filePath);
@@ -117,7 +118,7 @@ internal sealed class ApiVersionParameterFilter : IOperationFilter
             return;
         }
 
-        var versionParam = operation.Parameters.FirstOrDefault(p => p.Name.Equals(ApiVersioning.QueryParameterKey, StringComparison.OrdinalIgnoreCase));
+        var versionParam = operation.Parameters.FirstOrDefault(p => p.Name?.Equals(ApiVersioning.QueryParameterKey, StringComparison.OrdinalIgnoreCase) == true);
         if (versionParam != null)
         {
             operation.Parameters.Remove(versionParam);
@@ -127,29 +128,41 @@ internal sealed class ApiVersionParameterFilter : IOperationFilter
 
 internal sealed class ModelBaseSchemaFilter : ISchemaFilter
 {
-    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    public void Apply(IOpenApiSchema schema, SchemaFilterContext context)
     {
         if (context.Type.IsSubclassOf(typeof(ModelBase)))
         {
             // The OpenApi schema for this type will by by default allow additional
             // properties because of the [JsonExtensionData] member. But that is only
             // there to fail when unrecognized fields are encountered during deserialization.
-            schema.AdditionalPropertiesAllowed = false;
+            if (schema is OpenApiSchema concreteSchema)
+            {
+                concreteSchema.AdditionalPropertiesAllowed = false;
+                concreteSchema.AdditionalProperties = null;
+            }
         }
     }
 }
 
 internal sealed class OpenApiExcludeSchemaFilter : ISchemaFilter
 {
-    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    public void Apply(IOpenApiSchema schema, SchemaFilterContext context)
     {
+        if (schema.Properties == null)
+        {
+            return;
+        }
+
         var excludedProperties = context.Type.GetProperties().Where(p => p.GetCustomAttribute<OpenApiExcludeAttribute>() != null);
 
         foreach (var excludedProperty in excludedProperties)
         {
             // casing does not match
-            var keyToRemove = schema.Properties.Keys.Single(k => k.Equals(excludedProperty.Name, StringComparison.OrdinalIgnoreCase));
-            schema.Properties.Remove(keyToRemove);
+            var keyToRemove = schema.Properties.Keys.SingleOrDefault(k => k.Equals(excludedProperty.Name, StringComparison.OrdinalIgnoreCase));
+            if (keyToRemove != null)
+            {
+                schema.Properties.Remove(keyToRemove);
+            }
         }
     }
 }
@@ -158,31 +171,53 @@ public static class EndpointConventionBuilderExtensions
 {
     public static TBuilder WithTagsQueryParameters<TBuilder>(this TBuilder builder) where TBuilder : IEndpointConventionBuilder
     {
-        return builder.WithOpenApi(c =>
-        {
-            // Specify that the query parameter "tag" is a "deep object"
-            // and can be used like this `tag[key1]=value1&tag[key2]=value2`.
-            c.Parameters.Add(new OpenApiParameter
-            {
-                Name = "tag",
-                In = ParameterLocation.Query,
-                Required = false,
-                Schema = new OpenApiSchema
-                {
-                    Type = "object",
-                    AdditionalProperties = new OpenApiSchema
-                    {
-                        Type = "string",
-                    },
-                },
-                Style = ParameterStyle.DeepObject,
-                Explode = true,
-            });
+        return builder.WithMetadata(new TagsQueryParameterMetadata());
+    }
+}
 
-            // For some reason the text is changed to "OK" when we implement this,
-            // so we need to set it back to "Success".
-            c.Responses["200"].Description = "Success";
-            return c;
+internal sealed class TagsQueryParameterMetadata;
+
+/// <summary>
+/// Adds the "tag" deep-object query parameter to operations marked with <see cref="TagsQueryParameterMetadata"/>.
+/// </summary>
+internal sealed class TagsQueryParameterOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var metadata = context.ApiDescription.ActionDescriptor.EndpointMetadata
+            .OfType<TagsQueryParameterMetadata>()
+            .FirstOrDefault();
+
+        if (metadata == null)
+        {
+            return;
+        }
+
+        // Specify that the query parameter "tag" is a "deep object"
+        // and can be used like this `tag[key1]=value1&tag[key2]=value2`.
+        operation.Parameters ??= [];
+        operation.Parameters.Add(new OpenApiParameter
+        {
+            Name = "tag",
+            In = ParameterLocation.Query,
+            Required = false,
+            Schema = new OpenApiSchema
+            {
+                Type = JsonSchemaType.Object,
+                AdditionalProperties = new OpenApiSchema
+                {
+                    Type = JsonSchemaType.String,
+                },
+            },
+            Style = ParameterStyle.DeepObject,
+            Explode = true,
         });
+
+        // For some reason the text is changed to "OK" when we implement this,
+        // so we need to set it back to "Success".
+        // if (operation.Responses?.TryGetValue("200", out var okResponse) == true && okResponse != null)
+        // {
+        //     okResponse.Description = "Success";
+        // }
     }
 }
